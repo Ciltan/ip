7ecb09d1bed0b871c44d5eccf818191ac38d94f4
Ciltan <tangxc2004@gmail.com>
2026-08-20 02:29:27 +0800

    Add support for storing and displaying tasks

--- src/main/java/Orion.java
+++ src/main/java/Orion.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Orion {
@@ -7,19 +9,28 @@ public class Orion {
             + "\\____/_/  /_/\\____/_/ /_/\n";
     private static final String LINE = "____________________________________________________________";
 
+    private static List<String> tasks = new ArrayList<>();
+
     public static void main(String[] args) {
         greet();
+        boolean running = true;
         Scanner scanner = new Scanner(System.in);
-        while (true) {
+        while (running) {
             System.out.print("\n> ");
             String command = scanner.nextLine();
-            if (command.equals("bye")) {
-                bye();
-                break;
-            } else {
-                System.out.println(LINE);
-                System.out.println(command);
-                System.out.println(LINE);
+            switch (command) {
+                case "list":
+                    listTasks();
+                    break;
+
+                case "bye":
+                    bye();
+                    running = false;
+                    break;
+
+                default:
+                    addTask(command);
+                    break;
             }
         }
     }
@@ -35,6 +46,21 @@ public class Orion {
     private static void bye() {
         System.out.println(LINE);
         System.out.println("Hope to see you again soon. Goodbye!");
+        System.out.println(LINE);
+    }
+
+    private static void addTask(String task) {
+        System.out.println(LINE);
+        tasks.add(task);
+        System.out.println("added: " + task);
+        System.out.println(LINE);
+    }
+
+    private static void listTasks() {
+        System.out.println(LINE);
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.println((i + 1) + ". " + tasks.get(i));
+        }
         System.out.println(LINE);
     }
 }
