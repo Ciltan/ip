8986ff7d427eb9a6f9a53f4b7edd27c377efaf09
Ciltan <tangxc2004@gmail.com>
2026-08-20 02:06:43 +0800

    Add support for echo

--- src/main/java/Orion.java
+++ src/main/java/Orion.java
@@ -1,16 +1,40 @@
+import java.util.Scanner;
+
 public class Orion {
+    private static final String BANNER = "  ____       _\n"
+            + " / __ \\_____(_)___  ____\n"
+            + "/ /_/ / ___/ / __ \\/ __ \\\n"
+            + "\\____/_/  /_/\\____/_/ /_/\n";
+    private static final String LINE = "____________________________________________________________";
+
     public static void main(String[] args) {
-        String banner = "  ____       _\n"
-                + " / __ \\_____(_)___  ____\n"
-                + "/ /_/ / ___/ / __ \\/ __ \\\n"
-                + "\\____/_/  /_/\\____/_/ /_/\n";
-        String line = "____________________________________________________________";
-        System.out.println(line);
-        System.out.println(banner);
+        greet();
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.print("\n> ");
+            String command = scanner.nextLine();
+            if (command.equals("bye")) {
+                bye();
+                break;
+            } else {
+                System.out.println(LINE);
+                System.out.println(command);
+                System.out.println(LINE);
+            }
+        }
+    }
+
+    private static void greet() {
+        System.out.println(LINE);
+        System.out.println(BANNER);
         System.out.println("Hello! I'm Orion, your friendly chatbot.");
         System.out.println("What can I do for you?");
-        System.out.println(line);
+        System.out.println(LINE);
+    }
+
+    private static void bye() {
+        System.out.println(LINE);
         System.out.println("Hope to see you again soon. Goodbye!");
-        System.out.println(line);
+        System.out.println(LINE);
     }
 }
