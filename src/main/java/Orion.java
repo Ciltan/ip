172b2282defed4bce8f5e8fb4973696f3f24a4ca
Ciltan <tangxc2004@gmail.com>
2026-08-20 01:33:50 +0800

    Rename chatbot to Orion

---
+++ src/main/java/Orion.java
@@ -0,0 +1,9 @@
+public class Orion {
+    public static void main(String[] args) {
+        String banner = "  ____       _\n"
+                + " / __ \\_____(_)___  ____\n"
+                + "/ /_/ / ___/ / __ \\/ __ \\\n"
+                + "\\____/_/  /_/\\____/_/ /_/\n";
+        System.out.println(banner);
+    }
+}
