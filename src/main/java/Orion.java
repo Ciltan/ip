639fe4a5d125244e50cb8c51f10e84b6fc65b95e
Ciltan <tangxc2004@gmail.com>
2026-08-20 01:41:07 +0800

    Add greeting

--- src/main/java/Orion.java
+++ src/main/java/Orion.java
@@ -4,6 +4,13 @@ public class Orion {
                 + " / __ \\_____(_)___  ____\n"
                 + "/ /_/ / ___/ / __ \\/ __ \\\n"
                 + "\\____/_/  /_/\\____/_/ /_/\n";
+        String line = "____________________________________________________________";
+        System.out.println(line);
         System.out.println(banner);
+        System.out.println("Hello! I'm Orion, your friendly chatbot.");
+        System.out.println("What can I do for you?");
+        System.out.println(line);
+        System.out.println("Hope to see you again soon. Goodbye!");
+        System.out.println(line);
     }
 }
