f20f493e2616212149f34843e502fb8b7ac68798
Ciltan <tangxc2004@gmail.com>
2026-08-27 02:50:27 +0800

    Extract Ui, TaskList, and Parser classes

---
+++ src/main/java/TaskList.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaskList {
+    private List<Task> tasks;
+
+    public TaskList(List<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    public TaskList() {
+        this.tasks = new ArrayList<>();
+    }
+
+    public void addTask(Task task) {
+        tasks.add(task);
+    }
+
+    public Task removeTask(int index) {
+        return tasks.remove(index);
+    }
+
+    public Task getTask(int index) {
+        return tasks.get(index);
+    }
+
+    public int getSize() {
+        return tasks.size();
+    }
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+}
---
+++ src/main/java/Ui.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class Ui {
+    private static final String BANNER = "  ____       _\n"
+            + " / __ \\_____(_)___  ____\n"
+            + "/ /_/ / ___/ / __ \\/ __ \\\n"
+            + "\\____/_/  /_/\\____/_/ /_/\n";
+    private static final String LINE = "____________________________________________________________";
+    private Scanner scanner;
+
+    public Ui() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    public String readCommand() {
+        System.out.print("\n> ");
+        return scanner.nextLine();
+    }
+
+    public void showWelcome() {
+        System.out.println(LINE);
+        System.out.println(BANNER);
+        System.out.println("Hello! I'm Orion, your friendly chatbot.");
+        System.out.println("What can I do for you?");
+        System.out.println(LINE);
+    }
+
+    public void showGoodbye() {
+        System.out.println(LINE);
+        System.out.println("Hope to see you again soon. Goodbye!");
+        System.out.println(LINE);
+    }
+
+    public void showLine() {
+        System.out.println(LINE);
+    }
+
+    public void showError(String message) {
+        System.out.println(LINE);
+        System.out.println(message);
+        System.out.println(LINE);
+    }
+
+    public void showMessage(String message) {
+        System.out.println(message);
+    }
+}
