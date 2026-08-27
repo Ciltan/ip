e1c442f6ebb6a4d1b3eb61cfabd4241346505675
Ciltan <tangxc2004@gmail.com>
2026-08-27 15:01:23 +0800

    Add tests for Deadline and TaskList

---
+++ src/test/java/orion/task/DeadlineTest.java
@@ -0,0 +1,22 @@
+package orion.task;
+
+import java.time.LocalDateTime;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class DeadlineTest {
+    @Test
+    public void toFileFormat_uncompletedDeadline_success() {
+        LocalDateTime date = LocalDateTime.of(2026, 9, 4, 18, 30);
+        Deadline deadline = new Deadline("submit assignment", date);
+        assertEquals("D | 0 | submit assignment | 2026-09-04 1830", deadline.toFileFormat());
+    }
+
+    @Test
+    public void toFileFormat_completedDeadline_success() {
+        LocalDateTime date = LocalDateTime.of(2026, 8, 26, 10, 0);
+        Deadline deadline = new Deadline("submit assignment", date, true); // isDone is true
+        assertEquals("D | 1 | submit assignment | 2026-08-26 1000", deadline.toFileFormat());
+    }
+}
